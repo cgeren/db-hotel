035d1ae1444686d1afe7a9d63fbf92b329a26d20
cgeren <calebgeren23@gmail.com>
2023-03-14 15:49:23 -0500

    Created functions for generating amenities csv/table file... a work in progress

--- src/data-generation/Generate.java
+++ src/data-generation/Generate.java
@@ -1,4 +1,11 @@
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 /**
  * Program to generate data for fields within the hotel DB
@@ -9,27 +16,79 @@ import java.io.*;
  */
 
 public class Generate {
+    public final int NUM_HOTELS = 19;
+
     public static void main(String[] args) {
-        
-    }
+        String amenitiesCSV = "amenities.csv";
+        Path amenitiesPath = Paths.get(amenitiesCSV);
+        ArrayList<ArrayList<String>> amenitiesList = new ArrayList<ArrayList<String>>();
+        ArrayList<String> tempList = new ArrayList<String>();
+        generateAmenities(0, amenitiesList, tempList);
 
-    public String convertToCSV(String[] data) {
-        return Stream.of(data)
-                .map(this::escapeSpecialCharacters)
-                .collect(Collectors.joining(","));
-    }
-
-    public String escapeSpecialCharacters(String data) {
-        String escapedData = data.replaceAll("\\R", " ");
-        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
-            data = data.replace("\"", "\"\"");
-            escapedData = "\"" + data + "\"";
+        try {
+            writeToCSV(amenitiesList, amenitiesPath);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return escapedData;
     }
 
-    public void writeToCSV(List<String[]> lines, Path path) {
+    /**
+     * https://www.java2novice.com/java-collections-and-util/arraylist/list-to-csv/
+     * @param list
+     * @return
+     */
+    public static String getListAsCsvString(ArrayList<String> list){
+        StringBuilder sb = new StringBuilder();
+        for(String str:list){
+            if(sb.length() != 0){
+                sb.append(",");
+            }
+            sb.append(str);
+        }
+        return sb.toString();
+    }
 
+    public static void writeToCSV(ArrayList<ArrayList<String>> lines, Path path) throws IOException {
+        File csvOutputFile = new File(path.toString());
+
+        System.out.println(path.toString());
+
+        FileWriter myWriter = new FileWriter(csvOutputFile);
+
+        for (int i = 0; i < lines.size(); i++) {
+            String convertedToCSV = getListAsCsvString(lines.get(i));
+            myWriter.write(convertedToCSV);
+
+            if (i != (lines.size() - 1)) {
+                myWriter.write(System.getProperty("line.separator"));
+            }
+        }
+
+        myWriter.close();
+    }
+
+    public static void generateAmenities(int i, ArrayList<ArrayList<String>> amenities, ArrayList<String> list) {
+        int maxAmenities = 4;
+
+        if (i == maxAmenities) {
+            amenities.add(list);
+            list.removeAll();
+            return;
+        }
+
+        list.add("N");
+        generateAmenities(i + 1, amenities, list);
+
+        list.remove(list.size() - 1);
+
+        list.add("Y");
+        generateAmenities(i + 1, amenities, list);
+    }
+
+    public static void prependID(ArrayList<ArrayList<String>> toPrepend) {
+        for (int i = 1; i < toPrepend.size() + 1; i++) {
+            toPrepend.get(i).add(0, Integer.toString(i));
+        }
     }
 }
 
